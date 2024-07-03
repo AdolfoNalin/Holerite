7f4ec743e7fb6544a83150831d70f91258b5a279
AdolfoNalin <nalinadolfo33@gmail.com>
2024-07-02 21:59:51 -0300

    Criando o objeto Address e o método de consulta na API

---
+++ Helpers/ConsultCEP.cs
@@ -0,0 +1,43 @@
+﻿using Holerite.br.pro.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holerite.Helpers
+{
+    public class ConsultCEP
+    {
+        #region ConsultAddress
+        public static Address Consult(string cep)
+        {
+			try
+			{
+                string url = "https://viacep.com.br/ws/" + cep + "/xml/";
+
+                DataSet ds = new DataSet();
+                ds.ReadXml(url);
+
+                Address obj = new Address()
+                {
+                    Street = ds.Tables[0].Rows[0]["logradouro"].ToString(),
+                    Neighborhood = ds.Tables[0].Rows[0]["bairro"].ToString(),
+                    City = ds.Tables[0].Rows[0]["localidade"].ToString(),
+                    Complement = ds.Tables[0].Rows[0]["complemento"].ToString(),
+                    State = ds.Tables[0].Rows[0]["uf"].ToString()
+                };
+
+                return obj;
+            }
+			catch (Exception ex)
+			{
+				Dialog.Message("CEP não encontrado, por favor coloque manualmente", "atenção");
+				return null;
+			}
+        }
+        #endregion
+    }
+}
---
+++ br.pro.MODEL/Address.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holerite.br.pro.MODEL
+{
+    public class Address
+    {
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Neighborhood { get; set; }
+        public string Street { get; set; }
+        public string HomeNumber { get; set; }
+        public string Complement { get; set; }
+    }
+}
