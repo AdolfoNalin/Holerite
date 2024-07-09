24af12e3fee6a8435973e9aef67cc09d160024c7
AdolfoNalin <nalinadolfo33@gmail.com>
2024-07-09 10:28:51 -0300

    Criando a classe MODEL do itemBudget

---
+++ br.pro.MODEL/ItemBudget.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Holerite.br.pro.MODEL
+{
+    public class ItemBudget
+    {
+        public int Cod { get; set; }
+        public int CodBudget { get; set; }
+        public int CodSeverce { get; set; }
+        public int Amount { get; set; }
+        public float Price { get; set; }
+        public float Subtotal { get; set; }
+    }
+}
