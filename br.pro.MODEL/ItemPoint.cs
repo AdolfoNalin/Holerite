87aae4208c34d72fa5c2ce415eb49a294fdd5a0c
AdolfoNalin <nalinadolfo33@gmail.com>
2024-07-11 07:46:09 -0300

    Criando o item_point e o resumo do ponto

---
+++ br.pro.MODEL/ItemPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Holerite.br.pro.MODEL
+{
+    public class ItemPoint
+    {
+        public int Cod { get; set; }
+        public int CodPoint { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime EntryTime { get; set; }
+        public DateTime LunchDeparture { get; set; }
+        public DateTime ExitTime { get; set; }
+        public DateTime ExtraEntry { get; set; }
+        public DateTime ExtraOutput { get; set; }
+    }
+}
---
+++ br.pro.MODEL/Overview.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holerite.br.pro.MODEL
+{
+    public class Overview
+    {
+        public int Cod { get; set; }
+        public int CodPoint { get; set; }
+        public DateTime TimeWork { get; set; }
+        public DateTime ExtraTime { get; set; }
+        public float MiissingMonth { get; set; }
+        public float Subtotal { get; set; }
+        public float INSS { get; set; }
+        public float OthersDes { get; set; }
+        public float WageFam { get; set; }
+        public float tri { get; set; }
+        public float VisaInspection { get; set; }
+    }
+}
