d03ba4086b576d52fd93cfc77be0934e6d9da8b8
AdolfoNalin <nalinadolfo33@gmail.com>
2024-07-11 07:31:47 -0300

    Criando as classes relacionadas do ponto

---
+++ br.pro.DAO/PointDAO.cs
@@ -0,0 +1,184 @@
+﻿using Holerite.br.pro.Connection;
+using Holerite.br.pro.MODEL;
+using Holerite.Helpers;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holerite.br.pro.DAO
+{
+    public class PointDAO
+    {
+        private MySqlConnection _connection;
+
+        public PointDAO()
+        {
+            _connection = new ConnectionFactory().getConnection();
+        }
+
+        #region Insert
+        /// <summary>
+        /// Cadastra o ponto no banco de dados
+        /// </summary>
+        /// <param name="obj"></param>
+        public void Insert(Point obj)
+        {
+            try
+            {
+                string sql = "INSERT INTO point (cod_company, cod_emp) VALUES(@cod_company, cod_emp)";
+
+                MySqlCommand cmd = new MySqlCommand(sql, _connection);
+                cmd.Parameters.AddWithValue("@cod_company", obj.CodCompany);
+                cmd.Parameters.AddWithValue("@cod_emp", obj.CodEmp);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                Dialog.Message($"Aconteceu um erro do tipo {ex.Message} com o caminho para {ex.StackTrace}", "atenção");
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        #endregion
+
+        #region Update
+        /// <summary>
+        /// Atualiza os dados do ponto
+        /// </summary>
+        /// <param name="obj"></param>
+        public void Update(Point obj)
+        {
+            try
+            {
+                string sql = "UPDATE point SET cod_company=@cod_company, cod_emp=@cod_emp  WHERE cod=@cod";
+
+                MySqlCommand cmd = new MySqlCommand( sql, _connection);
+                cmd.Parameters.AddWithValue("@cod_company", obj.CodCompany);
+                cmd.Parameters.AddWithValue("@cod_emp", obj.CodEmp);
+                cmd.Parameters.AddWithValue("@cod", obj.Cod);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+
+
+            }
+            catch (Exception ex)
+            {
+                Dialog.Message($"Aconteceu um erro do tipo {ex.Message} com o caminho para {ex.StackTrace}", "atenção");
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        #endregion
+
+        #region Delete
+        /// <summary>
+        /// Delete a o ponto do banco de dados
+        /// </summary>
+        /// <param name="cod"></param>
+        public void Delete(int cod)
+        {
+            try
+            {
+                string sql = "DELETE FROM point WHERE cod=@cod";
+
+                MySqlCommand cmd = new MySqlCommand(sql, _connection);
+                cmd.Parameters.AddWithValue("@cod", cod);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+
+                Dialog.Message("Ponto foi deletado com sucesso", "sucesso");
+            }
+            catch (Exception ex)
+            {
+                Dialog.Message($"Aconteceu um erro do tipo {ex.Message} com o caminho para {ex.StackTrace}", "atenção");
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        #endregion
+
+        #region Consult
+        /// <summary>
+        /// Consulta todos os ponto 
+        /// </summary>
+        /// <returns></returns>
+        public DataTable Consult()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "SELECT * FROM point";
+                MySqlCommand cmd = new MySqlCommand(sql, _connection);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Dialog.Message($"Aconteceu um erro do tipo {ex.Message} com o caminho para {ex.StackTrace}", "Atenção");
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        #endregion
+
+        #region ConsultName
+        /// <summary>
+        /// Consulta os pontos pelo nome do funcionário
+        /// </summary>
+        /// <param name="emp_name"></param>
+        /// <returns></returns>
+        public DataTable Consult(string emp_name)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "SELECT * FROM point AS p JOIN user_employee AS u ON (p.cod_emp = u.cod) WHERE u.emp_name=@emp_name";
+
+                MySqlCommand cmd = new MySqlCommand(sql, _connection);
+                cmd.Parameters.AddWithValue("@emp_name", emp_name);
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Dialog.Message("Aconteceu um erro do tipo {ex.Message} com o caminho para {ex.StackTrace}", "atenção");
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        #endregion
+    }
+}
---
+++ br.pro.MODEL/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holerite.br.pro.MODEL
+{
+    public class Point
+    {
+        public int Cod { get; set; }
+        public int CodCompany { get; set; }
+        public int CodEmp { get; set; }
+    }
+}
