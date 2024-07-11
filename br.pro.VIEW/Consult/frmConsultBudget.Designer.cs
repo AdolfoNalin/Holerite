624f1fa729f400da6251ec33aca950c9d25b7144
AdolfoNalin <nalinadolfo33@gmail.com>
2024-07-11 07:58:31 -0300

    Criando a tela de orçamento

---
+++ br.pro.VIEW/Consult/frmConsultBudget.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Holerite.br.pro.VIEW.Consult
+{
+    partial class frmConsultBudget
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCod = new System.Windows.Forms.Label();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.dgBudget = new System.Windows.Forms.DataGridView();
+            this.txtNameClient = new System.Windows.Forms.TextBox();
+            this.lblNameClient = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.btnSeach = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBudget)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCod
+            // 
+            this.lblCod.AutoSize = true;
+            this.lblCod.Location = new System.Drawing.Point(18, 15);
+            this.lblCod.Name = "lblCod";
+            this.lblCod.Size = new System.Drawing.Size(40, 13);
+            this.lblCod.TabIndex = 0;
+            this.lblCod.Text = "Código";
+            // 
+            // txtCod
+            // 
+            this.txtCod.Location = new System.Drawing.Point(59, 12);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(41, 20);
+            this.txtCod.TabIndex = 1;
+            this.txtCod.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCod_KeyPress);
+            // 
+            // dgBudget
+            // 
+            this.dgBudget.AllowUserToAddRows = false;
+            this.dgBudget.AllowUserToDeleteRows = false;
+            this.dgBudget.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgBudget.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBudget.Location = new System.Drawing.Point(12, 134);
+            this.dgBudget.Name = "dgBudget";
+            this.dgBudget.ReadOnly = true;
+            this.dgBudget.Size = new System.Drawing.Size(776, 304);
+            this.dgBudget.TabIndex = 2;
+            // 
+            // txtNameClient
+            // 
+            this.txtNameClient.Location = new System.Drawing.Point(225, 12);
+            this.txtNameClient.Name = "txtNameClient";
+            this.txtNameClient.Size = new System.Drawing.Size(195, 20);
+            this.txtNameClient.TabIndex = 5;
+            this.txtNameClient.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNameClient_KeyPress);
+            // 
+            // lblNameClient
+            // 
+            this.lblNameClient.AutoSize = true;
+            this.lblNameClient.Location = new System.Drawing.Point(134, 15);
+            this.lblNameClient.Name = "lblNameClient";
+            this.lblNameClient.Size = new System.Drawing.Size(85, 13);
+            this.lblNameClient.TabIndex = 4;
+            this.lblNameClient.Text = "Nome do Cliente";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Data Início:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(533, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Data Fim:";
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpStart.Location = new System.Drawing.Point(589, 16);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(96, 20);
+            this.dtpStart.TabIndex = 9;
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpEnd.Location = new System.Drawing.Point(588, 39);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(96, 20);
+            this.dtpEnd.TabIndex = 10;
+            // 
+            // btnSeach
+            // 
+            this.btnSeach.Location = new System.Drawing.Point(701, 13);
+            this.btnSeach.Name = "btnSeach";
+            this.btnSeach.Size = new System.Drawing.Size(75, 23);
+            this.btnSeach.TabIndex = 11;
+            this.btnSeach.Text = "Pesquisar";
+            this.btnSeach.UseVisualStyleBackColor = true;
+            this.btnSeach.Click += new System.EventHandler(this.btnSeach_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Pesquisar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmConsultBudget
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSeach);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNameClient);
+            this.Controls.Add(this.lblNameClient);
+            this.Controls.Add(this.dgBudget);
+            this.Controls.Add(this.txtCod);
+            this.Controls.Add(this.lblCod);
+            this.Name = "frmConsultBudget";
+            this.Text = "Consulta Orçamento";
+            this.Load += new System.EventHandler(this.frmConsultBudget_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgBudget)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCod;
+        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.DataGridView dgBudget;
+        private System.Windows.Forms.TextBox txtNameClient;
+        private System.Windows.Forms.Label lblNameClient;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.Button btnSeach;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ br.pro.VIEW/Consult/frmConsultBudget.cs
@@ -0,0 +1,58 @@
+﻿using Holerite.br.pro.DAO;
+using Holerite.br.pro.MODEL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Holerite.br.pro.VIEW.Consult
+{
+    public partial class frmConsultBudget : Form
+    {
+        public frmConsultBudget()
+        {
+            InitializeComponent();
+        }
+
+        #region Load
+        private void frmConsultBudget_Load(object sender, EventArgs e)
+        {
+            BudgetDAO dao = new BudgetDAO();
+            dgBudget.DataSource = dao.Consult();
+        }
+
+        #endregion
+
+        #region txtCod_keypress
+        private void txtCod_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                dgBudget.DataSource = new BudgetDAO().Consult(int.Parse(txtCod.Text));
+            }
+        }
+        #endregion
+
+        #region tbnNameClient_keypress
+        private void txtNameClient_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            dgBudget.DataSource = new BudgetDAO().Consult(txtNameClient.Text);
+        }
+        #endregion
+
+        #region btnSearch for Date
+        private void btnSeach_Click(object sender, EventArgs e)
+        {
+            DateTime startDate = DateTime.Parse(dtpStart.Text);
+            DateTime endtDate = DateTime.Parse(dtpEnd.Text);
+
+            dgBudget.DataSource = new BudgetDAO().Consult(startDate, endtDate);
+        }
+        #endregion
+    }
+}
