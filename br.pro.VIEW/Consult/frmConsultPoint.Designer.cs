bd1c2c2e8b4e4b2e9c7607ba5b179cb9506409e4
AdolfoNalin <nalinadolfo33@gmail.com>
2024-07-17 07:02:09 -0300

    Criando a tela de consulta de ponto

---
+++ br.pro.VIEW/Consult/frmConsultPoint.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Holerite.br.pro.VIEW.Consult
+{
+    partial class frmConsultPoint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dgPoint = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPoint)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(31, 42);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nome:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(72, 39);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(303, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // dgPoint
+            // 
+            this.dgPoint.AllowUserToAddRows = false;
+            this.dgPoint.AllowUserToDeleteRows = false;
+            this.dgPoint.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgPoint.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPoint.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dgPoint.Location = new System.Drawing.Point(34, 106);
+            this.dgPoint.Name = "dgPoint";
+            this.dgPoint.ReadOnly = true;
+            this.dgPoint.Size = new System.Drawing.Size(745, 332);
+            this.dgPoint.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(381, 38);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Pesquisar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // frmConsultPoint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dgPoint);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmConsultPoint";
+            this.Text = "Consulta Ponto";
+            this.Load += new System.EventHandler(this.frmConsultPoint_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPoint)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DataGridView dgPoint;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
---
+++ br.pro.VIEW/Consult/frmConsultPoint.cs
@@ -0,0 +1,29 @@
+﻿using Holerite.br.pro.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Holerite.br.pro.VIEW.Consult
+{
+    public partial class frmConsultPoint : Form
+    {
+        public frmConsultPoint()
+        {
+            InitializeComponent();
+        }
+
+        #region Laod
+        private void frmConsultPoint_Load(object sender, EventArgs e)
+        {
+            PointDAO dao = new PointDAO();
+            dgPoint.DataSource = dao.Consult();
+        }
+        #endregion
+    }
+}
