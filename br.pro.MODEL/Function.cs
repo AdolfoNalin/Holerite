f4f4d83cc365f47d553fe4f2c418ba4819b5cdda
AdolfoNalin <nalinadolfo33@gmail.com>
2024-07-30 08:47:04 -0300

    Criando class modelo para função do empregado

---
+++ br.pro.MODEL/Function.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holerite.br.pro.MODEL
+{
+    public class Function
+    {
+        public int Cod { get; set; }
+        public string Fun{ get; set; }
+    }
+}
