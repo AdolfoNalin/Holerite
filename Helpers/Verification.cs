af849d2e7c6eea2c5447de1c98c9f613a2d558fd
AdolfoNalin <nalinadolfo33@gmail.com>
2024-07-30 19:02:39 -0300

    Criando o método login
    Método responsável por validar o login do usuário

---
+++ Helpers/Verification.cs
@@ -0,0 +1,66 @@
+﻿using Holerite.br.pro.DAO;
+using Holerite.br.pro.MODEL;
+using Holerite.br.pro.VIEW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holerite.Helpers
+{
+    public class Verification
+    {
+        #region
+        public void ToCheck()
+        {
+            
+        }
+        #endregion
+
+        #region
+        public void Tocheck(Employee obj)
+        {
+
+        }
+        #endregion
+
+        #region
+        public void Tocheck(Client obj)
+        {
+
+        }
+        #endregion
+
+        #region Varification User and password
+        /// <summary>
+        /// Verification function if o user_name is user
+        /// </summary>
+        /// <param name="userName">Name of user</param>
+        /// <param name="password">Name of Password</param>
+        public static void Login(string userName, string password)
+        {
+            EmployeeDAO dao = new EmployeeDAO();
+            try
+            {
+                if (dao.Login(userName, password))
+                {
+                    frmMenu tela = new frmMenu();
+                    tela.ShowDialog();
+                }
+                else
+                {
+                    Dialog.Message("Usuário não encontrado, tente novamente", "atenção");
+                    frmLogin tela = new frmLogin();
+                    tela.ShowDialog();
+                }
+            }
+            catch (Exception ex)
+            {
+                Dialog.Message($"Aconteceu um erro do tipo {ex.Message} com o caominho para {ex.StackTrace}", "Atenção");
+            }
+        }
+        #endregion
+    }
+}
