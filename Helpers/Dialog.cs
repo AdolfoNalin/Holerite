507d74d1865edf322c494df05e4a53efb117a298
AdolfoNalin <nalinadolfo33@gmail.com>
2024-08-07 09:52:00 -0300

    Criando métodos: MessageError, MessageInsert e MessageInsertOthers
    Métodos de messagens

--- Helpers/Dialog.cs
+++ Helpers/Dialog.cs
@@ -20,5 +20,35 @@ namespace Holerite.Helpers
             MessageBox.Show(message, title.ToUpper(), MessageBoxButtons.OK);
         }
         #endregion
+
+        #region MessageError
+        /// <summary>
+        /// Mensagem de erro para Catch
+        /// </summary>
+        /// <param name="ex"></param>
+        public static void MessageError(Exception ex)
+        {
+            MessageBox.Show($"Acontenceu um erro do tipo {ex.Message} com caminho para {ex.StackTrace}", "ATENÇÃO", MessageBoxButtons.OK);
+        }
+        #endregion
+
+        #region MessageInsert
+        /// <summary>
+        /// Mensagem de sucesso de cadastro
+        /// </summary>
+        /// <param name="objects"></param>
+        public static void MessageInsert(string objects, string title = "sucesso")
+        {
+            MessageBox.Show($"O {objects} foi cadastrado com sucesso", title.ToUpper(), MessageBoxButtons.OK);
+        }
+        #endregion
+
+        #region MessageInsertOthers
+        public static DialogResult MessageInsertOthers(string objects, string title = "atenção")
+        {
+            DialogResult resp = MessageBox.Show($"Deseja cadastrar outro {objects}?", title.ToUpper(), MessageBoxButtons.YesNo);
+            return resp;
+        }
+        #endregion
     }
 }
