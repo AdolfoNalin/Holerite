2160ee50ffcef81ba1b5443cf518433ba4edec31
AdolfoNalin <nalinadolfo33@gmail.com>
2024-08-07 11:21:41 -0300

    Add the members Month

--- br.pro.MODEL/Point.cs
+++ br.pro.MODEL/Point.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Holerite.br.pro.MODEL
 {
@@ -11,5 +12,6 @@ namespace Holerite.br.pro.MODEL
         public int Cod { get; set; }
         public int CodCompany { get; set; }
         public int CodEmp { get; set; }
+        public DateTime Month { get; set; }
     }
 }
