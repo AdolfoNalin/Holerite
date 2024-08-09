71e784b69821f6e0b5d0a71343c2400e535463c4
AdolfoNalin <nalinadolfo33@gmail.com>
2024-08-09 15:01:01 -0300

    Criando class modelo Epi

---
+++ br.pro.MODEL/Epi.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holerite.br.pro.MODEL
+{
+    public class Epi
+    {
+        public int Cod { get; set; }
+        public DateTime Date { get; set; }
+        public int CodEmp { get; set; }
+        public int CodProd { get; set; }
+        public int Amount { get; set; }
+        public string Obs { get; set; }
+    }
+}
