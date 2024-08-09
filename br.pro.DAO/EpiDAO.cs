a6974b124a0cd8c6765499506ca243c21c898a5f
AdolfoNalin <nalinadolfo33@gmail.com>
2024-08-09 15:13:54 -0300

    Criando os métodos Consult, ConsultName and SearchName

--- br.pro.DAO/EpiDAO.cs
+++ br.pro.DAO/EpiDAO.cs
@@ -2,8 +2,10 @@
 using Holerite.br.pro.MODEL;
 using Holerite.Helpers;
 using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Mozilla;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -72,6 +74,125 @@ namespace Holerite.br.pro.DAO
             catch (Exception ex)
             {
                 Dialog.MessageError(ex);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        #endregion
+
+        #region Consult
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public DataTable Consult()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = @"SELECT 
+                e.cod AS 'Código',
+                e.date AS 'Data',
+                u.emp_name AS 'Nome do Funcionário'
+                FROM epi AS e
+                JOIN product AS p ON (p.cod = e.cod_prod)
+                JOIN user_employee AS u ON (u.cod = e.cod_emp)";
+
+                MySqlCommand cmd = new MySqlCommand(sql, _connection);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Dialog.MessageError(ex);
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        #endregion
+
+        #region ConsultName
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public DataTable Consult(string name)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = @"SELECT 
+                e.cod AS 'Código',
+                e.date AS 'Data',
+                u.emp_name AS 'Nome do Funcionário'
+                FROM epi AS e
+                JOIN product AS p ON (p.cod = e.cod_prod)
+                JOIN user_employee AS u ON (u.cod = e.cod_emp) WHERE u.emp_name LIKE @name";
+
+                MySqlCommand cmd = new MySqlCommand(sql,_connection); 
+                cmd.Parameters.AddWithValue("@name", name);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Dialog.MessageError(ex);
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        #endregion
+
+        #region SearchName
+        public DataTable Search(string name)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = @"SELECT 
+                e.cod AS 'Código',
+                e.date AS 'Data',
+                u.emp_name AS 'Nome do Funcionário'
+                FROM epi AS e
+                JOIN product AS p ON (p.cod = e.cod_prod)
+                JOIN user_employee AS u ON (u.cod = e.cod_emp) WHERE u.name_emp=@name";
+
+                MySqlCommand cmd = new MySqlCommand(sql, _connection);
+                cmd.Parameters.AddWithValue("@name", name);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter();
+                da.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Dialog.MessageError(ex);
+                return null;
             }
             finally
             {
