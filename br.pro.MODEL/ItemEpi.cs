67438a891e6bdb4a908783de92ba1d89134a988e
AdolfoNalin <nalinadolfo33@gmail.com>
2024-08-10 14:21:32 -0300

    Remodelando o Epi, dividindo para a classe itemEpi

--- br.pro.MODEL/ItemEpi.cs
+++ br.pro.MODEL/ItemEpi.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.SqlServer.Server;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace Holerite.br.pro.MODEL
 {
-    public class Epi
+    public class ItemEpi
     {
         public int Cod { get; set; }
-        public DateTime Date { get; set; }
-        public int CodEmp { get; set; }
+        public int CodEpi { get; set; }
+        public int CodProd { get; set; }
+        public float Price { get; set; }
+        public int Amount { get; set; }
         public float Subtotal { get; set; }
         public string Obs { get; set; }
     }
