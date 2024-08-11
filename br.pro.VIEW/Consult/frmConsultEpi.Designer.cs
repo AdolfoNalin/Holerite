9f516e2948d6638b099c83174997d42d26da3bb4
AdolfoNalin <nalinadolfo33@gmail.com>
2024-08-11 13:51:43 -0300

    Criando a tela Consulta epi

---
+++ br.pro.VIEW/Consult/frmConsultEpi.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Holerite.br.pro.VIEW.Consult
+{
+    partial class frmConsultEpi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgEpi = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEpi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(41, 44);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nome:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(82, 41);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(237, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(324, 39);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Pesquisa ";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgEpi
+            // 
+            this.dgEpi.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgEpi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgEpi.Location = new System.Drawing.Point(44, 126);
+            this.dgEpi.Name = "dgEpi";
+            this.dgEpi.Size = new System.Drawing.Size(723, 295);
+            this.dgEpi.TabIndex = 3;
+            this.dgEpi.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgEpi_CellClick);
+            // 
+            // frmConsultEpi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgEpi);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmConsultEpi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consulta EPI";
+            this.Load += new System.EventHandler(this.frmConsultEpi_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgEpi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgEpi;
+    }
+}
---
+++ br.pro.VIEW/Consult/frmConsultEpi.cs
@@ -0,0 +1,64 @@
+﻿using Holerite.br.pro.DAO;
+using Holerite.br.pro.VIEW.Insert;
+using Org.BouncyCastle.Asn1.Cmp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Holerite.br.pro.VIEW.Consult
+{
+    public partial class frmConsultEpi : Form
+    {
+        public frmConsultEpi()
+        {
+            InitializeComponent();
+        }
+
+        #region Load
+        private void frmConsultEpi_Load(object sender, EventArgs e)
+        {
+            dgEpi.DataSource = new EpiDAO().Consult();
+        }
+        #endregion
+
+        #region txtName_KeyPress
+        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string name = "%" + txtName.Text + "%";
+
+            dgEpi.DataSource = new EpiDAO().Consult(name);
+        }
+        #endregion
+
+        #region btnSearch_click
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            dgEpi.DataSource = new EpiDAO().Search(txtName.Text);
+        }
+        #endregion
+
+        #region dgEpi_CellClick
+        private void dgEpi_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ItemEpiDAO dao = new ItemEpiDAO();
+            frmInsertEPI tela = new frmInsertEPI();
+            tela.rbSpot.Checked =  dgEpi.CurrentRow.Cells[3].Value.ToString() == "vista" ? true : tela.rbTerm.Checked = false;
+
+            int cod = int.Parse(dgEpi.CurrentRow.Cells[0].Value.ToString());
+            tela.txtCodEpi.Text = cod.ToString();
+            tela.mtbDate.Text = dgEpi.CurrentRow.Cells[1].Value.ToString();
+            tela.cbEmployee.Text = dgEpi.CurrentRow.Cells[2].Value.ToString();
+            tela.txtTotal.Text = dgEpi.CurrentRow.Cells[4].Value.ToString();
+            tela.dgEPI.DataSource = dao.Search(cod);
+            this.Hide();
+            tela.ShowDialog();
+        }
+        #endregion
+    }
+}
