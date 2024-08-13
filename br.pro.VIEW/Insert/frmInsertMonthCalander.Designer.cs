9a766b910fc87531f6cf3b69589733b8e9e6b5bd
AdolfoNalin <nalinadolfo33@gmail.com>
2024-08-13 13:57:48 -0300

    Craindo tela MonthCalendar
    Forma de selecionar os dias do ponto

---
+++ br.pro.VIEW/Insert/frmInsertMonthCalander.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Holerite.br.pro.VIEW.Insert
+{
+    partial class frmInsertMonthCalander
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mcControlDay = new System.Windows.Forms.MonthCalendar();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mcControlDay
+            // 
+            this.mcControlDay.Location = new System.Drawing.Point(18, 18);
+            this.mcControlDay.Name = "mcControlDay";
+            this.mcControlDay.TabIndex = 0;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(70, 196);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(95, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Gerar";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // frmInsertMonthCalander
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 231);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.mcControlDay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmInsertMonthCalander";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastrar Calendário";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGenerate;
+        public System.Windows.Forms.MonthCalendar mcControlDay;
+    }
+}
