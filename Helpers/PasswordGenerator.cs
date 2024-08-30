84d2c459b97bf85043fec53a24d2c0734b46d372
AdolfoNalin <nalinadolfo33@gmail.com>
2024-08-30 19:38:14 -0300

    Criando class password generator
    Class que possui o método GeneratePassword, método que cria um hash da senha do usuário

---
+++ Helpers/PasswordGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holerite.Helpers
+{
+    public class PasswordGenerator
+    {
+       public static string GeneratePassword(string password)
+       {
+            string hashpassword;
+            using (SHA256 pass = SHA256.Create())
+            {
+                byte[] bPassword = Encoding.UTF8.GetBytes(password);
+                byte[] bytes = pass.ComputeHash(bPassword);
+                hashpassword = Encoding.UTF8.GetString(bytes);
+            }
+
+            return hashpassword;
+       }
+    }
+}
