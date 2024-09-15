64f058f150e9cfcbaad0da885fc198b284953195
AdolfoNalin <nalinadolfo33@gmail.com>
2024-09-15 20:58:00 -0300

    Corrigindo erro no comando sql

---
+++ br.pro.VIEW/Consult/frmConsultService.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Holerite.br.pro.VIEW.Consult
+{
+    partial class frmConsultService
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.dgSeverce = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSeverce)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(111, 97);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(252, 20);
+            this.txtName.TabIndex = 0;
+            this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(73, 101);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name:";
+            // 
+            // dgSeverce
+            // 
+            this.dgSeverce.AllowUserToAddRows = false;
+            this.dgSeverce.AllowUserToDeleteRows = false;
+            this.dgSeverce.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgSeverce.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgSeverce.GridColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dgSeverce.Location = new System.Drawing.Point(12, 157);
+            this.dgSeverce.Name = "dgSeverce";
+            this.dgSeverce.ReadOnly = true;
+            this.dgSeverce.Size = new System.Drawing.Size(776, 281);
+            this.dgSeverce.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(374, 97);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(66, 20);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Pesquisar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // frmConsultSeverce
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dgSeverce);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmConsultSeverce";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consulta Serviço";
+            this.Load += new System.EventHandler(this.frmConsultSeverce_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgSeverce)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnSearch;
+        public System.Windows.Forms.DataGridView dgSeverce;
+    }
+}
