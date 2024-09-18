49f806a6e2568dee41fb2ba18d32ea9310fe495a
AdolfoNalin <nalinadolfo33@gmail.com>
2024-09-17 22:45:01 -0300

    Criando a tela permissão para usuário/employee

---
+++ br.pro.VIEW/Interface/frmPermission.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Holerite.br.pro.VIEW.Interface
+{
+    partial class frmPermission
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.cbClient = new System.Windows.Forms.CheckBox();
+            this.cbCompany = new System.Windows.Forms.CheckBox();
+            this.cbEmployee = new System.Windows.Forms.CheckBox();
+            this.cbSupplier = new System.Windows.Forms.CheckBox();
+            this.cbService = new System.Windows.Forms.CheckBox();
+            this.cbProduct = new System.Windows.Forms.CheckBox();
+            this.cbPoint = new System.Windows.Forms.CheckBox();
+            this.cbHolerite = new System.Windows.Forms.CheckBox();
+            this.cbCheckout = new System.Windows.Forms.CheckBox();
+            this.cbSetting = new System.Windows.Forms.CheckBox();
+            this.cbBudget = new System.Windows.Forms.CheckBox();
+            this.cbAdmin = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(26, 181);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(98, 31);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Voltar";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(213, 181);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(98, 31);
+            this.btnConfirm.TabIndex = 2;
+            this.btnConfirm.Text = "Confirmar";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // cbClient
+            // 
+            this.cbClient.AutoSize = true;
+            this.cbClient.Location = new System.Drawing.Point(26, 12);
+            this.cbClient.Name = "cbClient";
+            this.cbClient.Size = new System.Drawing.Size(58, 17);
+            this.cbClient.TabIndex = 3;
+            this.cbClient.Text = "Cliente";
+            this.cbClient.UseVisualStyleBackColor = true;
+            // 
+            // cbCompany
+            // 
+            this.cbCompany.AutoSize = true;
+            this.cbCompany.Location = new System.Drawing.Point(26, 35);
+            this.cbCompany.Name = "cbCompany";
+            this.cbCompany.Size = new System.Drawing.Size(67, 17);
+            this.cbCompany.TabIndex = 4;
+            this.cbCompany.Text = "Empresa";
+            this.cbCompany.UseVisualStyleBackColor = true;
+            // 
+            // cbEmployee
+            // 
+            this.cbEmployee.AutoSize = true;
+            this.cbEmployee.Location = new System.Drawing.Point(26, 58);
+            this.cbEmployee.Name = "cbEmployee";
+            this.cbEmployee.Size = new System.Drawing.Size(81, 17);
+            this.cbEmployee.TabIndex = 5;
+            this.cbEmployee.Text = "Funcionário";
+            this.cbEmployee.UseVisualStyleBackColor = true;
+            // 
+            // cbSupplier
+            // 
+            this.cbSupplier.AutoSize = true;
+            this.cbSupplier.Location = new System.Drawing.Point(26, 81);
+            this.cbSupplier.Name = "cbSupplier";
+            this.cbSupplier.Size = new System.Drawing.Size(80, 17);
+            this.cbSupplier.TabIndex = 6;
+            this.cbSupplier.Text = "Fornecedor";
+            this.cbSupplier.UseVisualStyleBackColor = true;
+            // 
+            // cbService
+            // 
+            this.cbService.AutoSize = true;
+            this.cbService.Location = new System.Drawing.Point(26, 104);
+            this.cbService.Name = "cbService";
+            this.cbService.Size = new System.Drawing.Size(62, 17);
+            this.cbService.TabIndex = 7;
+            this.cbService.Text = "Serviço";
+            this.cbService.UseVisualStyleBackColor = true;
+            // 
+            // cbProduct
+            // 
+            this.cbProduct.AutoSize = true;
+            this.cbProduct.Location = new System.Drawing.Point(26, 127);
+            this.cbProduct.Name = "cbProduct";
+            this.cbProduct.Size = new System.Drawing.Size(63, 17);
+            this.cbProduct.TabIndex = 8;
+            this.cbProduct.Text = "Produto";
+            this.cbProduct.UseVisualStyleBackColor = true;
+            // 
+            // cbPoint
+            // 
+            this.cbPoint.AutoSize = true;
+            this.cbPoint.Location = new System.Drawing.Point(213, 12);
+            this.cbPoint.Name = "cbPoint";
+            this.cbPoint.Size = new System.Drawing.Size(54, 17);
+            this.cbPoint.TabIndex = 9;
+            this.cbPoint.Text = "Ponto";
+            this.cbPoint.UseVisualStyleBackColor = true;
+            // 
+            // cbHolerite
+            // 
+            this.cbHolerite.AutoSize = true;
+            this.cbHolerite.Location = new System.Drawing.Point(213, 35);
+            this.cbHolerite.Name = "cbHolerite";
+            this.cbHolerite.Size = new System.Drawing.Size(62, 17);
+            this.cbHolerite.TabIndex = 10;
+            this.cbHolerite.Text = "Holerite";
+            this.cbHolerite.UseVisualStyleBackColor = true;
+            // 
+            // cbCheckout
+            // 
+            this.cbCheckout.AutoSize = true;
+            this.cbCheckout.Location = new System.Drawing.Point(213, 58);
+            this.cbCheckout.Name = "cbCheckout";
+            this.cbCheckout.Size = new System.Drawing.Size(52, 17);
+            this.cbCheckout.TabIndex = 11;
+            this.cbCheckout.Text = "Caixa";
+            this.cbCheckout.UseVisualStyleBackColor = true;
+            // 
+            // cbSetting
+            // 
+            this.cbSetting.AutoSize = true;
+            this.cbSetting.Location = new System.Drawing.Point(213, 81);
+            this.cbSetting.Name = "cbSetting";
+            this.cbSetting.Size = new System.Drawing.Size(94, 17);
+            this.cbSetting.TabIndex = 12;
+            this.cbSetting.Text = "Configurações";
+            this.cbSetting.UseVisualStyleBackColor = true;
+            // 
+            // cbBudget
+            // 
+            this.cbBudget.AutoSize = true;
+            this.cbBudget.Location = new System.Drawing.Point(213, 104);
+            this.cbBudget.Name = "cbBudget";
+            this.cbBudget.Size = new System.Drawing.Size(78, 17);
+            this.cbBudget.TabIndex = 13;
+            this.cbBudget.Text = "Orçamento";
+            this.cbBudget.UseVisualStyleBackColor = true;
+            // 
+            // cbAdmin
+            // 
+            this.cbAdmin.AutoSize = true;
+            this.cbAdmin.Location = new System.Drawing.Point(213, 127);
+            this.cbAdmin.Name = "cbAdmin";
+            this.cbAdmin.Size = new System.Drawing.Size(91, 17);
+            this.cbAdmin.TabIndex = 15;
+            this.cbAdmin.Text = "Adiministrador";
+            this.cbAdmin.UseVisualStyleBackColor = true;
+            this.cbAdmin.CheckedChanged += new System.EventHandler(this.cbAdmin_CheckedChanged);
+            // 
+            // frmPermission
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 227);
+            this.Controls.Add(this.cbAdmin);
+            this.Controls.Add(this.cbBudget);
+            this.Controls.Add(this.cbSetting);
+            this.Controls.Add(this.cbCheckout);
+            this.Controls.Add(this.cbHolerite);
+            this.Controls.Add(this.cbPoint);
+            this.Controls.Add(this.cbProduct);
+            this.Controls.Add(this.cbService);
+            this.Controls.Add(this.cbSupplier);
+            this.Controls.Add(this.cbEmployee);
+            this.Controls.Add(this.cbCompany);
+            this.Controls.Add(this.cbClient);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.btnBack);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmPermission";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Permissões";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.CheckBox cbClient;
+        private System.Windows.Forms.CheckBox cbCompany;
+        private System.Windows.Forms.CheckBox cbEmployee;
+        private System.Windows.Forms.CheckBox cbSupplier;
+        private System.Windows.Forms.CheckBox cbService;
+        private System.Windows.Forms.CheckBox cbProduct;
+        private System.Windows.Forms.CheckBox cbPoint;
+        private System.Windows.Forms.CheckBox cbHolerite;
+        private System.Windows.Forms.CheckBox cbCheckout;
+        private System.Windows.Forms.CheckBox cbSetting;
+        private System.Windows.Forms.CheckBox cbBudget;
+        private System.Windows.Forms.CheckBox cbAdmin;
+    }
+}
---
+++ br.pro.VIEW/Interface/frmPermission.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Holerite.br.pro.VIEW.Interface
+{
+    public partial class frmPermission : Form
+    {
+        public List<CheckBox> listChck { get; set; }
+
+        public frmPermission()
+        {
+            InitializeComponent();
+        }
+
+        #region btnBack_Click
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        #endregion
+
+        #region btnConfirm_click
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            List<CheckBox> cb = new List<CheckBox>()
+            {
+                cbClient, cbCompany, cbEmployee,
+                cbSupplier, cbService, cbProduct,
+                cbPoint, cbHolerite, cbCheckout,
+                cbSetting, cbBudget
+            };
+
+            listChck = cb.Where(lc => lc.Checked == true).ToList<CheckBox>();
+            this.Hide();
+        }
+        #endregion
+
+        #region cbAdmin_CheckedChanged
+        private void cbAdmin_CheckedChanged(object sender, EventArgs e)
+        {
+            List<CheckBox> cb = new List<CheckBox>()
+            {
+                cbClient, cbCompany, cbEmployee, 
+                cbSupplier, cbService, cbProduct, 
+                cbPoint, cbHolerite, cbCheckout, 
+                cbSetting, cbBudget
+            };
+
+            if(cbAdmin.Checked)
+            {
+                cb.ForEach(b => b.Checked = true);
+            }
+            else
+            {
+                cb.ForEach(c => c.Checked = false);
+            }
+        }
+        #endregion
+    }
+}
