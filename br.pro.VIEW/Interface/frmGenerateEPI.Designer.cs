d9fe57765ac10d3ff5f1d241d9d5af71833c5852
AdolfoNalin <nalinadolfo33@gmail.com>
2024-10-14 21:19:21 -0300

    Criando o gerar EPI

---
+++ br.pro.VIEW/Interface/frmGenerateEPI.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Holerite.br.pro.VIEW.Interface
+{
+    partial class frmGenerateEPI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtLine = new System.Windows.Forms.TextBox();
+            this.lblLine = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(89, 114);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Gerar";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 43);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Nome:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(53, 40);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(149, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(208, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 23);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtLine
+            // 
+            this.txtLine.Location = new System.Drawing.Point(53, 76);
+            this.txtLine.Name = "txtLine";
+            this.txtLine.Size = new System.Drawing.Size(41, 20);
+            this.txtLine.TabIndex = 4;
+            // 
+            // lblLine
+            // 
+            this.lblLine.AutoSize = true;
+            this.lblLine.Location = new System.Drawing.Point(12, 80);
+            this.lblLine.Name = "lblLine";
+            this.lblLine.Size = new System.Drawing.Size(36, 13);
+            this.lblLine.TabIndex = 5;
+            this.lblLine.Text = "Linha:";
+            // 
+            // frmGenerateEPI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 167);
+            this.Controls.Add(this.lblLine);
+            this.Controls.Add(this.txtLine);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnGenerate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmGenerateEPI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerar EPI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtLine;
+        private System.Windows.Forms.Label lblLine;
+    }
+}
---
+++ br.pro.VIEW/Interface/frmGenerateEPI.cs
@@ -0,0 +1,32 @@
+﻿using Holerite.br.pro.DAO;
+using Holerite.br.pro.MODEL;
+using Holerite.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Holerite.br.pro.VIEW.Interface
+{
+    public partial class frmGenerateEPI : Form
+    {
+        public frmGenerateEPI()
+        {
+            InitializeComponent();
+        }
+
+        #region btnGenerate_Click
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Employee emp = new EmployeeDAO().GetSearchEmp(txtName.Text);
+            int.TryParse(txtLine.Text, out int amountLine);
+            PrintOut.PrintOutEpi(emp, amountLine);
+        }
+        #endregion
+    }
+}
